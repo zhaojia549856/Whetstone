7da568a00741d283609ba87ca8c76e37d958cd9c
Jiajia Zhao <jzhao29@vols.utk.edu>
2019-06-26 00:11:23 -0400

    -_-

--- benchmark/adaptive_cifar10.py
+++ benchmark/adaptive_cifar10.py
@@ -75,7 +75,8 @@ def print_debug_spike(model, x_test, y_test):
 
 	    for x in range(len(x_test[i])):
 	    	for y in range(len(x_test[i][x])):
-	    		f_spike.write("%f " % x_test[i][x][y])  
+	    		for z in range(len(x_test[i][x][y]))
+	    		f_spike.write("%f " % x_test[i][x][y][z])  
 	    f_spike.write("%d %d\n" % (X[i], answer)) 
 
 	    for j in range(len(model.layers)):
